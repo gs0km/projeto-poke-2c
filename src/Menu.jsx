49204777429317ecb273a8b7be41efff8a2b08cc
gs0km <gf3326309@gmail.com>
2025-06-26 11:27:52 +0000

    ViaCep

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -8,6 +8,7 @@ const Menu = () => {
                 <Navbar.Toggle aria-controls="main-navbar-nav" />
                 <Navbar.Collapse id="main-navbar-nav">
                     <Nav className="me-auto">
+                        <Nav.Link href='/ViaCep'>ViaCep</Nav.Link>
                         <Nav.Link href="/home" active>Home</Nav.Link>
                         <Nav.Link href="/pokeflex">PokeFlex</Nav.Link>
                         <Nav.Link href="/pokegrid">PokeGrid</Nav.Link>
